4fc41d7b2d29379212ddd48d9e22bc8632ec57a1
杨晓明 <yangxiaoming512@yeah.net>
2019-03-30 21:50:19 +0800

    updat4

--- build/release.sh
+++ build/release.sh
@@ -38,7 +38,7 @@ then
   git push origin master
   git push origin refs/tags/v$VERSION
   git checkout dev
-  git rebase master
+  git rebase master 
   git push origin dev
 
   # if [[ $VERSION =~ "beta" ]]
