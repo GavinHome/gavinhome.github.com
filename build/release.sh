44c3b766bb4c6a435c40a4afda6aeeacd969ce22
杨晓明 <yangxiaoming512@yeah.net>
2019-03-30 21:58:54 +0800

    [build] 0.0.1

--- build/release.sh
+++ build/release.sh
@@ -8,6 +8,7 @@ VERSION=`npx select-version-cli`
 
 read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
+echo $REPLY
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
   echo "Releasing $VERSION ..."
@@ -36,11 +37,11 @@ then
 
   # publish
   echo $VERSION
-  # git push origin master
-  # git push origin refs/tags/v$VERSION
-  # git checkout dev
-  # git rebase master 
-  # git push origin dev
+  git push origin master
+  git push origin refs/tags/v$VERSION
+  git checkout dev
+  git rebase master 
+  git push origin dev
 
   # if [[ $VERSION =~ "beta" ]]
   # then
